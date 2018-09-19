abff1edf32b079c9cb3974f91681409faf637252
Mark Rushakoff <mark@influxdata.com>
2018-09-19 15:29:37 -0700

    Add fuzz test for AST Copy

---
+++ flux/parser_NewAST_Copy/fuzz.go
@@ -0,0 +1,22 @@
+package parser_NewAST
+
+import (
+	"reflect"
+
+	"github.com/influxdata/flux/parser"
+)
+
+func Fuzz(data []byte) int {
+	ast, err := parser.NewAST(string(data))
+	if err != nil {
+		// Not interesting.
+		return 0
+	}
+
+	c1 := ast.Copy()
+	c2 := c1.Copy()
+	if !reflect.DeepEqual(c1, c2) {
+		panic("copy of copy changed")
+	}
+	return 0
+}
